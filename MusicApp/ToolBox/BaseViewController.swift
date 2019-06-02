d098e868622d33de8b813645b8b721e9ba25cb07
bashirsk <user.email>
2019-06-02 13:02:33 +0100

    BaseViewController was set up

---
+++ MusicApp/ToolBox/BaseViewController.swift
@@ -0,0 +1,63 @@
+//
+//  BaseViewController.swift
+//  MusicApp
+//
+//  Created by Bashir Sentongo on 02/06/2019.
+//  Copyright © 2019 Bashir Sentongo. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let theTableView = self.tableView else { return }
+        theTableView.mp_setUpTableView(with: self)
+    }
+    
+    // MARK:- UIViewController control set up
+    
+    /**
+     - These are set up at the beginning incase they are needed later
+     */
+    
+    func doDismiss(completion pCompletion: (() -> Void)? = nil) {
+        self.presentingViewController?.dismiss(animated: true, completion: pCompletion)
+    }
+    
+    func pushViewController(with pViewController: UIViewController) {
+        self.navigationController?.pushViewController(pViewController, animated: true)
+    }
+    
+    
+    // MARK:- TableView set up
+    
+    var tableViewArray: [[Any]] {
+        return []
+    }
+    
+    func itemForIdenxPath(indexPath pIndexPath: IndexPath) -> Any {
+        return self.tableViewArray[pIndexPath.section][pIndexPath.row]
+    }
+}
+
+//MARK:- UITableViewDelegate, UITableViewDataSource
+
+extension BaseViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in pTableView: UITableView) -> Int {
+       return self.tableViewArray.count
+    }
+    
+    func tableView(_ pTableView: UITableView, numberOfRowsInSection pSection: Int) -> Int {
+        return self.tableViewArray[pSection].count
+        
+    }
+    
+    func tableView(_ pTableView: UITableView, cellForRowAt pIndexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
